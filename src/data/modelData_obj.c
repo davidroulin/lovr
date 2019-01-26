3d54082fc012b86db89800c49963537832b09e84
bjorn <tie.372@gmail.com>
2019-01-26 14:18:16 -0800

    Load obj geometry;

--- src/data/modelData_obj.c
+++ src/data/modelData_obj.c
@@ -1,4 +1,5 @@
 #include "data/modelData.h"
+#include "lib/math.h"
 #include <stdio.h>
 
 #define STARTS_WITH(a, b) !strncmp(a, b, strlen(b))
@@ -57,15 +58,15 @@ ModelData* lovrModelDataInitObj(ModelData* model, Blob* source, ModelDataIO io) 
 
             // Can be improved
             if (sscanf(s, "%d/%d/%d", &v, &vt, &vn) == 3) {
-              vec_pusharr(&vertexBuffer, vertices.data + 3 * v, 3);
-              vec_pusharr(&vertexBuffer, normals.data + 3 * vn, 3);
-              vec_pusharr(&vertexBuffer, uvs.data + 2 * vn, 2);
+              vec_pusharr(&vertexBuffer, vertices.data + 3 * (v - 1), 3);
+              vec_pusharr(&vertexBuffer, normals.data + 3 * (vn - 1), 3);
+              vec_pusharr(&vertexBuffer, uvs.data + 2 * (vt - 1), 2);
             } else if (sscanf(s, "%d//%d", &v, &vn) == 2) {
-              vec_pusharr(&vertexBuffer, vertices.data + 3 * v, 3);
-              vec_pusharr(&vertexBuffer, normals.data + 3 * vn, 3);
+              vec_pusharr(&vertexBuffer, vertices.data + 3 * (v - 1), 3);
+              vec_pusharr(&vertexBuffer, normals.data + 3 * (vn - 1), 3);
               vec_pusharr(&vertexBuffer, ((float[2]) { 0 }), 2);
             } else if (sscanf(s, "%d", &v) == 1) {
-              vec_pusharr(&vertexBuffer, vertices.data + 3 * v, 3);
+              vec_pusharr(&vertexBuffer, vertices.data + 3 * (v - 1), 3);
               vec_pusharr(&vertexBuffer, ((float[5]) { 0 }), 5);
             } else {
               lovrThrow("Bad OBJ: Unknown face format");
@@ -75,7 +76,7 @@ ModelData* lovrModelDataInitObj(ModelData* model, Blob* source, ModelDataIO io) 
           s = space + 1;
         }
       }
-      lineLength = s - data + 1;
+      lineLength = s - data;
     } else {
       char* newline = memchr(data, '\n', length);
       lineLength = newline - data + 1;
@@ -87,12 +88,85 @@ ModelData* lovrModelDataInitObj(ModelData* model, Blob* source, ModelDataIO io) 
 
   model->blobCount = 2;
   model->blobs = calloc(model->blobCount, sizeof(Blob*));
-  model->blobs[0] = lovrBlobCreate(vertexBuffer.data, vertexBuffer.capacity, "obj vertex data");
-  model->blobs[1] = lovrBlobCreate(indexBuffer.data, indexBuffer.capacity, "obj index data");
+  model->blobs[0] = lovrBlobCreate(vertexBuffer.data, vertexBuffer.length * sizeof(float), "obj vertex data");
+  model->blobs[1] = lovrBlobCreate(indexBuffer.data, indexBuffer.length * sizeof(int), "obj index data");
+
+  model->bufferCount = 2;
+  model->buffers = calloc(model->bufferCount, sizeof(ModelBuffer));
+
+  model->buffers[0] = (ModelBuffer) {
+    .data = model->blobs[0]->data,
+    .size = model->blobs[0]->size,
+    .stride = 8 * sizeof(float)
+  };
+
+  model->buffers[1] = (ModelBuffer) {
+    .data = model->blobs[1]->data,
+    .size = model->blobs[1]->size,
+    .stride = sizeof(int)
+  };
+
+  model->attributeCount = 4;
+  model->attributes = calloc(model->attributeCount, sizeof(ModelAttribute));
+
+  model->attributes[0] = (ModelAttribute) {
+    .buffer = 0,
+    .offset = 0,
+    .count = vertexBuffer.length / 8,
+    .type = F32,
+    .components = 3
+  };
+
+  model->attributes[1] = (ModelAttribute) {
+    .buffer = 0,
+    .offset = 3 * sizeof(float),
+    .count = vertexBuffer.length / 8,
+    .type = F32,
+    .components = 3
+  };
+
+  model->attributes[2] = (ModelAttribute) {
+    .buffer = 0,
+    .offset = 6 * sizeof(float),
+    .count = vertexBuffer.length / 8,
+    .type = F32,
+    .components = 2
+  };
+
+  model->attributes[3] = (ModelAttribute) {
+    .buffer = 1,
+    .offset = 0,
+    .count = indexBuffer.length,
+    .type = U32,
+    .components = 1
+  };
+
+  model->primitiveCount = 1;
+  model->primitives = calloc(model->primitiveCount, sizeof(ModelPrimitive));
+  model->primitives[0] = (ModelPrimitive) {
+    .mode = DRAW_TRIANGLES,
+    .attributes = {
+      [ATTR_POSITION] = &model->attributes[0],
+      [ATTR_NORMAL] = &model->attributes[1],
+      [ATTR_TEXCOORD] = &model->attributes[2]
+    },
+    .indices = &model->attributes[3],
+    .material = -1
+  };
+
+  model->nodeCount = 1;
+  model->nodes = calloc(model->nodeCount, sizeof(ModelNode));
+  model->nodes[0] = (ModelNode) {
+    .transform = MAT4_IDENTITY,
+    .primitiveIndex = 0,
+    .primitiveCount = 1
+  };
+
+  model->rootNode = 0;
 
   map_deinit(&vertexMap);
   vec_deinit(&vertices);
   vec_deinit(&normals);
   vec_deinit(&uvs);
-  return NULL;
+  return model;
 }
